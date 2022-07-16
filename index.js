ced75df29bab3441a0e486e31135ade749d92d4d
unknown <LENOVO@RYCTADMDEV-BCP.adm.RedandYellow.co.za>
2022-07-16 12:35:25 +0200

    playing index

--- index.js
+++ index.js
@@ -71,3 +71,19 @@ function debounce(func, timeout = 300){
      }
  });
 
+
+
+
+
+
+ const express = require('express');
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send("Welcome to masagwadi")
+})
+
+const port = process.env.port || 4000;
+app.listen(port,()=>{
+    console.log("wazzup");
+});
