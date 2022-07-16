112c987416ef5354e4e4e15d0684e25685b26ada
unknown <LENOVO@RYCTADMDEV-BCP.adm.RedandYellow.co.za>
2022-07-16 13:11:51 +0200

    PORT

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.get('/',(req,res)=>{
     res.send("Welcome to masagwadi")
 })
 
-const port = process.env.port || 4000;
+const port = process.env.PORT || 4000;
 app.listen(port,()=>{
     console.log("wazzup");
 });
